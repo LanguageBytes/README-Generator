6c27db68fdef78abc553f32012156a2e93912083
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-02-24 11:50:39 +0000

    testing license

--- generatedREADME.md
+++ generatedREADME.md
@@ -1,21 +1,22 @@
 
   
-  # h
+  # s
 
   ## Description 
   s
   s
   s
-  s
-
-  ## Installation 
   ss
 
+  ## Installation 
+  s
+
   ## Usage 
-  sss
+  s
 
   ## Collaborators
   ss
 
   ## License 
    apache
+   '[![License](https://img.shields.io/badge/License-apache_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,4 @@
 // Generate Markdown based on questions answered
-
-
 const generateMarkdown = data => {
   return `
   
@@ -23,18 +21,10 @@ const generateMarkdown = data => {
 
   ## License 
    ${data.license}
+   '[![License](https://img.shields.io/badge/License-${data.license}_1.0-lightblue.svg)]'
 `;
 }
 
-const generateLicense = data => {
-  if (data.license === apache)
-  {
-"[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
- if (data.license === boost)
- '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
-  }
-  else {""}
-}
 
-module.exports = generateLicense;
+
 module.exports = generateMarkdown;
